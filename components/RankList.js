e59d0fabb12f08b09c3d38545de5b7c8fc88311a
Priyansh Saxena <askpriyansh@gmail.com>
2020-03-29 15:30:01 +0530

    issue #1: Add basic ranklist (#2)
    Closes #1

---
+++ components/RankList.js
@@ -0,0 +1,137 @@
+import React from 'react';
+import { withStyles, makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
+const StyledTableCell = withStyles((theme) => ({
+  head: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  body: {
+    fontSize: 14,
+  },
+}))(TableCell);
+
+const StyledTableRow = withStyles((theme) => ({
+  root: {
+    '&:nth-of-type(odd)': {
+      backgroundColor: theme.palette.background.default,
+    },
+  },
+}))(TableRow);
+
+function setData(input) {
+    input.sort(function(a, b) {
+        return b.score - a.score;
+    });
+    return input.map(function(person, index) {
+        return createData(person.name, person.score, person.setBonus, person.idBonus, index+1);
+    });
+
+}
+
+function createData(name, score, setBonus, idBonus, rank) {
+  return { name, score, setBonus, idBonus, rank };
+}
+
+const rows = setData([
+    {
+        "name": "Ritam Deb",
+        "score": 2,
+        "setBonus": 0,
+        "idBonus": 0
+    },
+    {
+        "name": "Sudeshchandra Mohana",
+        "score": 5,
+        "setBonus": 0,
+        "idBonus": 0
+    },
+    {
+        "name": "Saurabh Maurya",
+        "score": 0,
+        "setBonus": 0,
+        "idBonus": 0
+    },
+    {
+        "name": "Ritam Deb",
+        "score": 2,
+        "setBonus": 0,
+        "idBonus": 0
+    },
+    {
+        "name": "Sudhakar Gajendran",
+        "score": 6,
+        "setBonus": 0,
+        "idBonus": 0
+    },
+    {
+        "name": "Ankhuri Dubey",
+        "score": 1,
+        "setBonus": 0,
+        "idBonus": 0
+    },
+    {
+        "name": "Vikram Narayan Chidambaram",
+        "score": 3,
+        "setBonus": 0,
+        "idBonus": 0
+    },
+    {
+        "name": "Ronak Jain",
+        "score": 7,
+        "setBonus": 0,
+        "idBonus": 0
+    },
+    {
+        "name": "Sandeep Chekuri",
+        "score": 0,
+        "setBonus": 0,
+        "idBonus": 0
+    }
+]);
+
+const useStyles = makeStyles({
+  table: {
+    minWidth: 700,
+  },
+});
+
+export default function RankList() {
+  const classes = useStyles();
+
+  return (
+    <TableContainer component={Paper}>
+      <Table className={classes.table} aria-label="customized table">
+        <TableHead>
+          <TableRow>
+            <StyledTableCell>Bored Panda</StyledTableCell>
+            <StyledTableCell align="right">Boredom</StyledTableCell>
+            <StyledTableCell align="right">Set Bonus</StyledTableCell>
+            <StyledTableCell align="right">ID Bonus</StyledTableCell>
+            <StyledTableCell align="right">Rank</StyledTableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <StyledTableRow key={row.name}>
+              <StyledTableCell component="th" scope="row">
+                {row.name}
+              </StyledTableCell>
+              <StyledTableCell align="right">{row.score}</StyledTableCell>
+              <StyledTableCell align="right">{row.setBonus}</StyledTableCell>
+              <StyledTableCell align="right">{row.idBonus}</StyledTableCell>
+              <StyledTableCell align="right">{row.rank}</StyledTableCell>
+            </StyledTableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,203 +1,181 @@
 import Head from 'next/head'
+import Header from '../components/Header';
+import React from 'react'
+import Layout from '../components/layout'
+import RankList from '../components/RankList'
 
-const Home = () => (
-  <div className="container">
-    <Head>
-      <title>Create Next App</title>
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
+export default class extends React.Component {
+  render() {
+    return (
+        <div className="container">
+            <Head>
+              <title>RankList</title>
+              <link rel="icon" href="/favicon.ico" />
+            </Head>
 
-    <main>
-      <h1 className="title">
-        Welcome to <a href="https://nextjs.org">Next.js!</a>
-      </h1>
+            <main>
+              <h1 className="title">
+                Welcome to <a href="/">Baby-Photos Ranklist</a>
+              </h1>
+              <RankList/>
+            </main>
 
-      <p className="description">
-        Get started by editing <code>pages/index.js</code>
-      </p>
+            <footer>
+              <a
+                href="https://zeit.co?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Powered by <img src="/zeit.svg" alt="ZEIT Logo" />
+              </a>
+            </footer>
 
-      <div className="grid">
-        <a href="https://nextjs.org/docs" className="card">
-          <h3>Documentation &rarr;</h3>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+            <style jsx>{`
+              .container {
+                min-height: 100vh;
+                padding: 0 0.5rem;
+                display: flex;
+                flex-direction: column;
+                justify-content: center;
+                align-items: center;
+              }
 
-        <a href="https://nextjs.org/learn" className="card">
-          <h3>Learn &rarr;</h3>
-          <p>Learn about Next.js in an interactive course with quizzes!</p>
-        </a>
+              main {
+                padding: 5rem 0;
+                flex: 1;
+                display: flex;
+                flex-direction: column;
+                justify-content: center;
+                align-items: center;
+              }
 
-        <a
-          href="https://github.com/zeit/next.js/tree/master/examples"
-          className="card"
-        >
-          <h3>Examples &rarr;</h3>
-          <p>Discover and deploy boilerplate example Next.js projects.</p>
-        </a>
+              footer {
+                width: 100%;
+                height: 100px;
+                border-top: 1px solid #eaeaea;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+              }
 
-        <a
-          href="https://zeit.co/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          className="card"
-        >
-          <h3>Deploy &rarr;</h3>
-          <p>
-            Instantly deploy your Next.js site to a public URL with ZEIT Now.
-          </p>
-        </a>
-      </div>
-    </main>
+              footer img {
+                margin-left: 0.5rem;
+              }
 
-    <footer>
-      <a
-        href="https://zeit.co?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Powered by <img src="/zeit.svg" alt="ZEIT Logo" />
-      </a>
-    </footer>
+              footer a {
+                display: flex;
+                justify-content: center;
+                align-items: center;
+              }
 
-    <style jsx>{`
-      .container {
-        min-height: 100vh;
-        padding: 0 0.5rem;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-      }
+              a {
+                color: inherit;
+                text-decoration: none;
+              }
 
-      main {
-        padding: 5rem 0;
-        flex: 1;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-      }
+              .title a {
+                color: #0070f3;
+                text-decoration: none;
+              }
 
-      footer {
-        width: 100%;
-        height: 100px;
-        border-top: 1px solid #eaeaea;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-      }
+              .title a:hover,
+              .title a:focus,
+              .title a:active {
+                text-decoration: underline;
+              }
 
-      footer img {
-        margin-left: 0.5rem;
-      }
+              .title {
+                margin: 0;
+                line-height: 1.15;
+                font-size: 4rem;
+                margin-bottom: 30px;
+              }
 
-      footer a {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-      }
+              .title,
+              .description {
+                text-align: center;
+              }
 
-      a {
-        color: inherit;
-        text-decoration: none;
-      }
+              .description {
+                line-height: 1.5;
+                font-size: 1.5rem;
+              }
 
-      .title a {
-        color: #0070f3;
-        text-decoration: none;
-      }
+              code {
+                background: #fafafa;
+                border-radius: 5px;
+                padding: 0.75rem;
+                font-size: 1.1rem;
+                font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
+                  DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
+              }
 
-      .title a:hover,
-      .title a:focus,
-      .title a:active {
-        text-decoration: underline;
-      }
+              .grid {
+                display: flex;
+                align-items: center;
+                justify-content: center;
+                flex-wrap: wrap;
 
-      .title {
-        margin: 0;
-        line-height: 1.15;
-        font-size: 4rem;
-      }
+                max-width: 800px;
+                margin-top: 3rem;
+              }
 
-      .title,
-      .description {
-        text-align: center;
-      }
+              .card {
+                margin: 1rem;
+                flex-basis: 45%;
+                padding: 1.5rem;
+                text-align: left;
+                color: inherit;
+                text-decoration: none;
+                border: 1px solid #eaeaea;
+                border-radius: 10px;
+                transition: color 0.15s ease, border-color 0.15s ease;
+              }
 
-      .description {
-        line-height: 1.5;
-        font-size: 1.5rem;
-      }
+              .card:hover,
+              .card:focus,
+              .card:active {
+                color: #0070f3;
+                border-color: #0070f3;
+              }
 
-      code {
-        background: #fafafa;
-        border-radius: 5px;
-        padding: 0.75rem;
-        font-size: 1.1rem;
-        font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-          DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-      }
+              .card h3 {
+                margin: 0 0 1rem 0;
+                font-size: 1.5rem;
+              }
 
-      .grid {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        flex-wrap: wrap;
+              .card p {
+                margin: 0;
+                font-size: 1.25rem;
+                line-height: 1.5;
+              }
 
-        max-width: 800px;
-        margin-top: 3rem;
-      }
+              .filtered-list .filter {
+                  margin-bottom: 21px;
+              }
 
-      .card {
-        margin: 1rem;
-        flex-basis: 45%;
-        padding: 1.5rem;
-        text-align: left;
-        color: inherit;
-        text-decoration: none;
-        border: 1px solid #eaeaea;
-        border-radius: 10px;
-        transition: color 0.15s ease, border-color 0.15s ease;
-      }
+              @media (max-width: 600px) {
+                .grid {
+                  width: 100%;
+                  flex-direction: column;
+                }
+              }
+            `}</style>
 
-      .card:hover,
-      .card:focus,
-      .card:active {
-        color: #0070f3;
-        border-color: #0070f3;
-      }
+            <style jsx global>{`
+              html,
+              body {
+                padding: 0;
+                margin: 0;
+                font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+                  Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+              }
 
-      .card h3 {
-        margin: 0 0 1rem 0;
-        font-size: 1.5rem;
-      }
-
-      .card p {
-        margin: 0;
-        font-size: 1.25rem;
-        line-height: 1.5;
-      }
-
-      @media (max-width: 600px) {
-        .grid {
-          width: 100%;
-          flex-direction: column;
-        }
-      }
-    `}</style>
-
-    <style jsx global>{`
-      html,
-      body {
-        padding: 0;
-        margin: 0;
-        font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
-          Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
-      }
-
-      * {
-        box-sizing: border-box;
-      }
-    `}</style>
-  </div>
-)
-
-export default Home
+              * {
+                box-sizing: border-box;
+              }
+            `}</style>
+          </div>
+    )
+  }
+}
